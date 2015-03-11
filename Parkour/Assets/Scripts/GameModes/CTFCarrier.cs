2a3e8c50c9a2b6a4466d7c80f8b40dc41df28718
Robert Rouhani <robert.rouhani@gmail.com>
2015-03-11 12:51:58 -0400

    Implemented CTF

---
+++ Parkour/Assets/Scripts/GameModes/CTFCarrier.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CTFCarrier : MonoBehaviour {
+
+	private bool hasFlag;
+
+	public bool HasFlag { get { return hasFlag; } }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnFlagPickup(CTFFlag flag) {
+		hasFlag = true;
+	}
+}
