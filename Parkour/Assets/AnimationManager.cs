ebf8f0ef8663ec31902738ea443d98f14f512056
Parshva Shah <g7parsh@gmail.com>
2015-03-11 13:10:10 -0400

    Started adding Collider to Body Geo, Tried to get parkour script to work with player got character to move started  animation manager

---
+++ Parkour/Assets/AnimationManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationManager : MonoBehaviour {
+	public Animator anim;
+	public GameObject player;
+	public CharacterController control;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		player = gameObject;
+		control = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
