a6ecc603dbd7fdda2a6534b82b6c96b62e6f7e57
Robert Rouhani <robert.rouhani@gmail.com>
2015-03-11 01:37:59 -0400

    Imported Photon Unity Networking, got player synchronization mostly working

---
+++ Parkour/Assets/Scripts/PlayerSpawn.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSpawn : MonoBehaviour {
+
+	public GameObject cameraObject;
+
+	void OnJoinedRoom()
+	{
+		Vector3 position = new Vector3(227.3f, 7.2f, 152.3f);
+		GameObject newPlayerObject = PhotonNetwork.Instantiate("Player", position, Quaternion.identity, 0);
+		cameraObject.transform.parent = newPlayerObject.transform;
+		Debug.Log ("CONNECTED");
+	}
+}
