b5c168963afa243b495f4c9e5bff3fd2e6612ab5
Robert Rouhani <robert.rouhani@gmail.com>
2015-04-01 12:35:22 -0400

    Extra networking changes

--- Parkour/Assets/Scripts/Movement/ParkourController.cs
+++ Parkour/Assets/Scripts/Movement/ParkourController.cs
@@ -301,8 +301,8 @@ public class ParkourController : MonoBehaviour {
 	}
 
 	void LateUpdate(){
-		if (photonView != null && !photonView.isMine)
-			return;
+		//if (photonView != null && !photonView.isMine)
+			//return;
 
 		netImpulse *= Time.deltaTime;
 
