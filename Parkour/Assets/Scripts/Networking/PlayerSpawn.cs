b606eceffff2b8a3d427fa544915dafba41af9b2
Robert Rouhani <robert.rouhani@gmail.com>
2015-04-01 11:46:19 -0400

    Fixed networking bugs

--- Parkour/Assets/Scripts/Networking/PlayerSpawn.cs
+++ Parkour/Assets/Scripts/Networking/PlayerSpawn.cs
@@ -3,19 +3,21 @@ using System.Collections;
 
 public class PlayerSpawn : MonoBehaviour {
 
-	public string attachName;
+	//public string attachName;
 	public GameObject cameraObject;
 	public Vector3 cameraAttachPos;
 	public Quaternion cameraAttachRot;
 
+	private int playerNum;
+
 	void OnJoinedRoom()
 	{
 		//Vector3 position = new Vector3(227.3f, 15.2f, 152.3f);
-		GameObject newPlayerObject = PhotonNetwork.Instantiate("Player", transform.position, Quaternion.identity, 0);
+		GameObject newPlayerObject = PhotonNetwork.Instantiate("Player", transform.position + Vector3.right * playerNum * 3f, Quaternion.identity, 0);
 		//newPlayerObject.GetComponent<CharacterController> ().enabled = false;
 
-		Transform attachObj = newPlayerObject.transform.Find (attachName) ?? newPlayerObject.transform;
-		cameraObject.transform.parent = attachObj;
+		//Transform attachObj = newPlayerObject.transform.Find (attachName) ?? newPlayerObject.transform;
+		cameraObject.transform.parent = newPlayerObject.transform;
 		cameraObject.transform.localPosition = cameraAttachPos;
 		cameraObject.transform.localRotation = cameraAttachRot;
 		//Debug.Log ("CONNECTED");
