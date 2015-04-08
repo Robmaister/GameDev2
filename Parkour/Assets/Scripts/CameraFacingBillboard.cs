c1df1e77f2d3d6f9608b7c783ad00df9ec0e2f54
Craig <cjcarlson20@gmail.com>
2015-04-08 17:12:44 -0400

    testing nametag

---
+++ Parkour/Assets/Scripts/CameraFacingBillboard.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFacingBillboard : MonoBehaviour
+{
+	public Camera m_Camera;
+
+	void Start(){
+		m_Camera = Camera.main;
+	}
+	
+	void Update()
+	{
+		if(m_Camera != null){
+			transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.back,
+			                 m_Camera.transform.rotation * Vector3.up);
+		}
+	}
+}
