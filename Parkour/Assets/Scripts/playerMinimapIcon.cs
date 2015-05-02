b3c28e86dfa31d43068de3b4d442e543c1d47c09
Craig <cjcarlson20@gmail.com>
2015-05-02 02:10:44 -0400

    added minimap

---
+++ Parkour/Assets/Scripts/playerMinimapIcon.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMinimapIcon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+
+		PhotonView pv = transform.root.GetComponentInChildren<PhotonView>();
+		CTFCarrier ctfc = transform.root.GetComponentInChildren<CTFCarrier>();
+		SpriteRenderer spr = GetComponent<SpriteRenderer>();
+		if(pv.isMine){
+			spr.color = Color.green;
+		}else{
+			if(ctfc.team == 0){
+				spr.color = Color.red;
+			}
+			else if(ctfc.team == 1){
+				spr.color = Color.blue;
+			}
+		}
+	}
+}
